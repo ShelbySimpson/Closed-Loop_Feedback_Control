b86d94a8743a6fc730ece018014dcdd0d114d3e7
SS-simpson <shelbywsimpson@gmail.com>
2016-10-12 08:27:44 -0700

    Transfer

--- LightTracking/LightTracking.ino
+++ LightTracking/LightTracking.ino
@@ -2,7 +2,7 @@
 //#include "LED.h"
 #include "LightSensor.h"
 //#include "Microphone.h"
-//#include "Slide.h"
+#include "Slide.h"
 #include "Switch.h"
 //#include "Button.h"
 #include "ColorLED.h"
@@ -14,18 +14,19 @@ short RED = 9;
 int posServo = 0;
 bool settingUp;
 short degreeInterval;
-short lightPrev = 0;//previous light reading
+short lightPrev;//previous light reading
 short lightCurr;//current light reading
 short nextMove = 0;
 short dir = 1;
 short posCurr = 180;//current position of servo(0-180)
+short lightThreshold = 100;//the point at which the area will be scanned for light source.
 //--------
 //Initate instances 
 //Thermometer therm = Thermometer();
 //LED leds[6] = {LED(4), LED(5), LED(6), LED(7), LED(8), LED(13)};
 LightSensor lightSensor = LightSensor();
 //Microphone mic = Microphone();
-//Slide slide = Slide();
+Slide slide = Slide();
 Switch swch = Switch();
 //Button bttn = Button();
 ColorLED cLED = ColorLED(RED);
@@ -39,12 +40,34 @@ bool isMove(short curr, short prev){
   return move;
 }
 
+short scanLow(){
+    cLED.off();
+    cLED.setColor(RED);//green mean its a go
+    cLED.on(255);//on and ready for flashlight movement.
+    servo.rotate(0);//set to intial testing position
+    short low;
+    lightPrev = 181;
+    for(short i = 0; i < 181; i = i + degreeInterval){
+      servo.rotate(i);//rotate at degree intervals to test light intensity.
+      delay(50);//allow time for servo movement
+      lightCurr = lightSensor.getData();//get light data at current degree.
+      if(lightCurr < lightPrev){//keep track of highest light degree reading
+        lightPrev = lightCurr;//current becomes previous as another data sample will be taken
+        low = i;
+        Serial.print("this is low - ");
+        Serial.println(low);
+      }
+    }
+    return low;
+}
+
 short scan(){
     cLED.off();
     cLED.setColor(RED);//green mean its a go
     cLED.on(255);//on and ready for flashlight movement.
     servo.rotate(0);//set to intial testing position
     short newPos = 0;
+    lightPrev = 0;
     for(short i = 0; i < 181; i = i + degreeInterval){
       servo.rotate(i);//rotate at degree intervals to test light intensity.
       delay(50);//allow time for servo movement
@@ -63,6 +86,16 @@ void setup() {
   settingUp = true;
   degreeInterval = 5;//light data sample based on degreeInterval
   servo.initAttach();
+  servo.rotate(0);
+  servo.rotate(scan());
+  lightThreshold = slide.getData();
+  Serial.print("Light Threshold - ");
+  Serial.println(lightThreshold);
+  Serial.println("Finished Setting Up");
+  Serial.print("this is low final - ");
+  Serial.println(scanLow());
+  delay(5000);
+  
   
 }
 
@@ -71,36 +104,45 @@ void loop() {
   //1.)find the degree to which light has the most intensity.
     //for each degree interval(0-180) find highest light sensor reading,sandbox scans inviroment.
     //this will have sandbox in a close enough proximity as long the intervals are reasonable.
-    if(swch.getData() == 1){
-    while(settingUp){
-    cLED.on(255);//turn red light on until finished setting up
-    servo.rotate(0);//set to intial testing position
-    for(short i = 0; i < 181; i = i + degreeInterval){
-      servo.rotate(i);//rotate at degree intervals to test light intensity.
-      delay(50);//allow time for servo movement
-      lightCurr = lightSensor.getData();//get light data at current degree.
-      if(lightCurr > lightPrev){//keep track of highest light degree reading
-        lightPrev = lightCurr;//current becomes previous as another data sample will be taken
-        posCurr = i;
-      }
-    }
-    servo.rotate(posCurr);//move to area with most light
-    //servo.rotate(90);//temp testing
-    delay(50);
-    settingUp = false;
+//    while(settingUp){
+////  servo.rotate(0);
+//  lightCurr = scan();
+//  servo.rotate(scan());
+//  Serial.println("Finished Setting Up");
+//      delay(5000);
+//    settingUp = false;
+//    }
     //cLED.off();
     //cLED.setColor(GREEN);//green mean its a go
     //cLED.on(255);//on and ready for flashlight movement.
-    Serial.println("Finished Setting Up");
-    }
+    
+
+//    while(settingUp){
+//    cLED.on(255);//turn red light on until finished setting up
+//    servo.rotate(0);//set to intial testing position
+//    for(short i = 0; i < 181; i = i + degreeInterval){
+//      servo.rotate(i);//rotate at degree intervals to test light intensity.
+//      delay(50);//allow time for servo movement
+//      lightCurr = lightSensor.getData();//get light data at current degree.
+//      if(lightCurr > lightPrev){//keep track of highest light degree reading
+//        lightPrev = lightCurr;//current becomes previous as another data sample will be taken
+//        posCurr = i;
+//      }
+//    }
+//    servo.rotate(posCurr);//move to area with most light
+//    //servo.rotate(90);//temp testing
+//    }
       //Begin looking for light changes--------------------------------------------------------------------------------------
-      //if light's reading is < 100 scan enviroment
+      //if light's reading is < lightThreshold scan enviroment
             lightCurr = lightSensor.getData();
       Serial.println(lightCurr);
-      if(lightCurr < 100){
+      if(lightCurr < lightThreshold){
+        //delay and try again to check if light source is back
         delay(1000);
         lightCurr = lightSensor.getData();
-          if(lightCurr < 100){
+          if(lightCurr < lightThreshold){
+            //light source is not back, scan for light
+            //if(scan() < lightThreshold){
             servo.rotate(scan()); 
             delay(50);
                 cLED.off();
@@ -123,5 +165,4 @@ void loop() {
           delay(50);
         }
       }
-    }
 }
